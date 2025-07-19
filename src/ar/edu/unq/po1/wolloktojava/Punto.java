d05b60951003a0b7edc8b141a69418a5417b89d0
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-07-19 02:42:47 -0300

    Version inicial

---
+++ src/ar/edu/unq/po1/wolloktojava/Punto.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.po1.wolloktojava;
+
+public class Punto {
+
+	private float x;
+	private float y;
+	
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public Punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+}
